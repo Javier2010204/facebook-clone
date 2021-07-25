1102389c5fb6a9ee9443790af49e0665eb7f0ef6
JavierSantaCruzXD <2010204javiergomez@gmail.com>
2021-07-25 16:21:50 -0600

    primer commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+
+  before_action :set_user
+  
+  def show
+    
+  end
+  
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:avatar, :username, :bio, :cover)
+    end
+    
+    
+end
