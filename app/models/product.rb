56843284cb4548c6eb907a7c4326abc998d6cb35
JavierSantaCruzXD <2010204javiergomez@gmail.com>
2021-08-09 23:39:26 -0600

    annotate gem for models annotations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :integer          not null, primary key
+#  description :text
+#  name        :string
+#  price       :decimal(, )
+#  state       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :integer          not null
+#
+# Indexes
+#
+#  index_products_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  user_id  (user_id => users.id)
+#
 class Product < ApplicationRecord
   belongs_to :user
   has_one_attached :cover
--- test/models/friendship_test.rb
+++ test/models/friendship_test.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: friendships
+#
+#  id         :integer          not null, primary key
+#  status     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  friend_id  :integer          not null
+#  user_id    :integer          not null
+#
+# Indexes
+#
+#  index_friendships_on_friend_id  (friend_id)
+#  index_friendships_on_user_id    (user_id)
+#
+# Foreign Keys
+#
+#  user_id  (user_id => users.id)
+#
 require 'test_helper'
 
 class FriendshipTest < ActiveSupport::TestCase
